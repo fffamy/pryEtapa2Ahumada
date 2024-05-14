71ea85938db08716ecfb42f72a48314788b9b84e
Federico Ahumada <fedee_crc@hotmail.com>
2024-05-14 05:25:42 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace pryEtapa2Ahumada
+{
+    partial class frmEtapa2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.pctImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(147, 349);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(204, 86);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // pctImagen
+            // 
+            this.pctImagen.Location = new System.Drawing.Point(73, 29);
+            this.pctImagen.Name = "pctImagen";
+            this.pctImagen.Size = new System.Drawing.Size(338, 267);
+            this.pctImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctImagen.TabIndex = 1;
+            this.pctImagen.TabStop = false;
+            // 
+            // frmEtapa2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 471);
+            this.Controls.Add(this.pctImagen);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "frmEtapa2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Etapa 2- CREAR OBJETOS POR CÓDIGO";
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.PictureBox pctImagen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEtapa2Ahumada
+{
+    public partial class frmEtapa2 : Form
+    {
+        private List<clsVehiculo> vehiculos;
+        private int indiceActual = 0;
+
+        public frmEtapa2()
+        {
+            InitializeComponent();
+
+            vehiculos = new List<clsVehiculo>
+            {
+                new clsVehiculo("Auto", "Auto", Properties.Resources.auto),
+                new clsVehiculo("Barco", "Barco", Properties.Resources.barco),
+                new clsVehiculo("Avion", "Avion", Properties.Resources.avion)
+            };
+        }
+
+        private void MostrarSiguienteVehiculo()
+        {
+
+            indiceActual = (indiceActual + 1) % vehiculos.Count;
+            clsVehiculo vehiculo = vehiculos[indiceActual];
+
+
+            pctImagen.Image = vehiculo.Imagen;
+            pctImagen.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            MostrarSiguienteVehiculo();
+        }
+    }
+}
